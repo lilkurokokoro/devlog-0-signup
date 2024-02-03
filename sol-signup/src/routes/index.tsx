c383dcb50fd22276606d9cbb48344c2df766ece7
Evgene Kuro <lilkurokokoro@gmail.com>
2024-02-04 02:17:57 +0300

    install solidstart

---
+++ sol-signup/src/routes/index.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main class="max-w-[800px] mx-auto text-gray-700 p-4 mt-16">
+      <h1 class="bold text-2xl mb-4">DEVLOG 0 SAMPLE SIGNUP FORM</h1>
+      <p><b>Task:</b> create sample signup form using this Figma file:&nbsp;
+        <a href="https://www.figma.com/file/7CvCi6oBU5QfBZcQbnVhXV/DEVLOG-0.-Sample-Sign-in-Form?type=design&node-id=0%3A1&mode=design&t=wdKYO76Ye15sLq8q-1" target="_blank">CLICK</a>
+      </p>
+      <p><b>Stack:</b></p>
+        <ul>
+          <li>
+            <a href="https://github.com/solidjs/solid-start">SolidStart</a>
+          </li>
+          <li>
+            <a href="https://github.com/tailwindlabs/tailwindcss">TailwindCSS</a>
+          </li>
+        </ul>
+
+    </main>
+  );
+}
