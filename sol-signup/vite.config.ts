89b4a8ec0a728aa94320fae741399643c1cd43a7
qrheart <evgeny.karhanov@yandex.ru>
2024-02-07 03:02:15 +0300

    fix the build

--- sol-signup/vite.config.ts
+++ sol-signup/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "@solidjs/start/config";
-import vercel from "solid-start-vercel";
-import solid from "solid-start/vite";
-
 export default defineConfig({
-  plugins: [solid({ adapter: vercel({ edge: true }) })],
+  start: {
+    ssr: false
+  }
 });
